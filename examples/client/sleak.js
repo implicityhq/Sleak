8928404ed4d9cb7f82b2a69ca154eb25502efe6b
Jason Silberman <jasonjt99@gmail.com>
2014-08-07 06:59:43 -0700

    Code Cleanup

--- examples/client/sleak.js
+++ examples/client/sleak.js
@@ -26,27 +26,23 @@ function createRandomString(len) {
   return text;
 }
 
-var timestamp = Math.floor(Date.now() / 1000),
+function createSleakHeader(unsortedParameters) {
+  var timestamp = Math.floor(Date.now() / 1000),
     nonce = createRandomString(8);
 
-var unPparams = {'type' : 'search', 'q' : 'watch companies'};
-var params = {};
-Object.keys(unPparams)
-      .sort()
-      .forEach(function(v, i) {
-          params[v] = unPparams[v];
-       });
-params['x-sleak-application-id'] = APP_ID;
-params['x-sleak-timestamp'] = timestamp;
-params['x-sleak-nonce'] = nonce;
+  var params = {};
+  Object.keys(unsortedParameters).sort().forEach(function(v, i) { params[v] = unsortedParameters[v]; });
+  params['x-sleak-application-id'] = APP_ID;
+  params['x-sleak-timestamp'] = timestamp;
+  params['x-sleak-nonce'] = nonce;
 
-console.log(params);
-console.log(queryStringValue(params));
-var hash = crypto.createHmac('sha256', PRIVATE_KEY).update(queryStringValue(params)).digest('hex');
-console.log(hash);
+  var hash = crypto.createHmac('sha256', PRIVATE_KEY).update(queryStringValue(params)).digest('hex');
 
-var authHeader = 'Authorization: Sleak';
-authHeader += ' ' + hash;
-authHeader += ', auth_nonce="' + nonce + '", auth_timestamp="' + timestamp + '"';
+  var authHeader = 'Authorization: Sleak';
+  authHeader += ' ' + hash;
+  authHeader += ', auth_nonce="' + nonce + '", auth_timestamp="' + timestamp + '"';
+  return authHeader;
+}
 
-console.log(authHeader);
+var header = createSleakHeader({'type' : 'search', 'q' : 'watch companies'});
+console.log(header);
